6d2105512d12bc7f566782db0ae25d7dda766771
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-13 17:34:49 +0000

    Remove unnecessary files

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
+class Dec2Hex {
+    public static void main(String args[]) {
+        if (args.length > 0) {
+            try {
+                int arg1 = Integer.parseInt(args[0]);
+                if (arg1 >= 0) {
+                    char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                    String hexadecimal = "";
 
-    public static int Arg1;
+                    System.out.println("Converting the Decimal Value " + arg1 + " to Hex...");
 
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+                    while (arg1 != 0) {
+                        int rem = arg1 % 16;
+                        hexadecimal = ch[rem] + hexadecimal;
+                        arg1 = arg1 / 16;
+                    }
 
-        while(num != 1)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+                    System.out.println("Hexadecimal representation is: " + hexadecimal);
+                } else {
+                    System.out.println("Invalid input. Please provide a non-negative integer.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please provide a valid integer.");
+            }
+        } else {
+            System.out.println("No input provided. Please provide an integer.");
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
     }
 }
