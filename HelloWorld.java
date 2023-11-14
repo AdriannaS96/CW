1b5617c1d5d43bba3935989b95f2813712373af6
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-14 17:59:15 +0000

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld {
 // Prints "Hello, World" to the terminal window
 public static void main(String args[])
 {
-System.out.println("Hello22, World");
+System.out.println("Hello23332, World");
 }
 }
