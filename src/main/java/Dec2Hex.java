9dd07ffe145be736e5519ea19cb254bc5022b9a6
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-15 11:31:35 +0000

    commit

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -1,9 +1,20 @@
 public class Dec2Hex {
-    public static String convertDecimalToHex(int decimalNumber) {
-        if (decimalNumber < 0) {
-            return "Please enter a non-negative number.";
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No input provided. Please provide an integer.");
+            return;
         }
 
+        try {
+            int decimalNumber = Integer.parseInt(args[0]);
+            String hexValue = convertToHex(decimalNumber);
+            System.out.println("Hexadecimal representation: " + hexValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide a valid decimal number.");
+        }
+    }
+
+    public static String convertToHex(int decimalNumber) {
         if (decimalNumber == 0) {
             return "0";
         }
@@ -19,21 +30,6 @@ public class Dec2Hex {
         }
 
         return hexBuilder.toString();
-    }
-
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("No input provided. Please provide an integer.");
-            return;
-        }
-
-        try {
-            int decimalNumber = Integer.parseInt(args[0]);
-            String hexValue = convertDecimalToHex(decimalNumber);
-            System.out.println("Hexadecimal representation: " + hexValue);
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please provide a valid decimal number.");
-        }
     }
 }
 
