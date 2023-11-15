87c0296b8098ab4b5a768aec07a3e638f2b960ea
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-15 12:00:17 +0000

    test22

--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -5,20 +5,28 @@ public class Dec2HexTest {
 
     @Test
     public void testDecimalToHexConversion() {
-        assertEquals("F", Dec2Hex.convertDecimalToHex(15));
-        assertEquals("1", Dec2Hex.convertDecimalToHex(1));
-        assertEquals("19", Dec2Hex.convertDecimalToHex(25));
-        assertEquals("1E", Dec2Hex.convertDecimalToHex(30));
-        assertEquals("6A", Dec2Hex.convertDecimalToHex(106));
-        assertEquals("0", Dec2Hex.convertDecimalToHex(0));
+        assertEqual("F", Dec2Hex.convertDecimalToHex(15));
+        assertEqual("1", Dec2Hex.convertDecimalToHex(1));
+        assertEqual("19", Dec2Hex.convertDecimalToHex(25));
+        assertEqual("1E", Dec2Hex.convertDecimalToHex(30));
+        assertEqual("6A", Dec2Hex.convertDecimalToHex(106));
+        assertEqual("0", Dec2Hex.convertDecimalToHex(0));
     }
 
     @Test
     public void testInvalidInput() {
-        assertEquals("Enter non-negative number.", Dec2Hex.convertDecimalToHex(-1));
+        assertEqual("Enter non-negative number.", Dec2Hex.convertDecimalToHex(-1));
+    }
+
+    private void assertEqual(String expected, String actual) {
+        try {
+            assertEquals(expected, actual);
+        } catch (AssertionError e) {
+            System.out.println("Assertion Error: " + e.getMessage());
+            throw e;
+        }
     }
 }
-
 
 
 
