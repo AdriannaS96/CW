e97ba07c087527ab9fedef0f3f1fbd7cdd643a5b
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-15 12:08:26 +0000

    m

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 
-class Dec2Hex {
+public class Dec2Hex {
 
-	public static String convertDecimalToHex(int decimal) {
-		if (decimal < 0) {
-            		return "Please enter a non negative number.";
-		}
-		
-		char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-		String hexadecimal = "";
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("No input provided. Please provide an integer.");
+            return;
+        }
 
-		while (decimal != 0) {
-			int rem = decimal % 16;
-			hexadecimal = ch[rem] + hexadecimal;
-			decimal = decimal / 16;
-		}
+        try {
+            int decimal = Integer.parseInt(args[0]);
+            if (decimal < 0) {
+                System.out.println("Enter non-negative number.");
+                return;
+            }
 
-		return hexadecimal;
-	}
+            String hexadecimal = convertDecimalToHex(decimal);
 
-	public static void main(String args[]) {
-		if (args.length == 0) {
-			System.out.println("Please provide an argument to the program. E.g. 15");
-			System.exit(0);
-		} else {
-			try {
-				for ( String arg : args ) {
-					int intArg = Integer.parseInt(arg);
-					String result = convertDecimalToHex(intArg);
-					System.out.println("Hexadecimal representation is: " + result);
-				}
-			}
-			catch (NumberFormatException e) {
-				System.out.println("Invalid argument. Please provide a valid integer.");
-				System.exit(0);
-			}
-		}
-	}
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide a valid integer.");
+        }
+    }
+
+    public static String convertDecimalToHex(int decimal) {
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        StringBuilder hexadecimal = new StringBuilder();
+
+        while (decimal != 0) {
+            rem = decimal % 16;
+            hexadecimal.insert(0, ch[rem]);
+            decimal = decimal / 16;
+        }
+
+        return hexadecimal.toString();
+    }
 }
+
 
 
 
